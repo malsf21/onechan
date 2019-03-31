4bee926ebacb227074f7afbac0050cd29d237a3b
Matthew Wang <matt@matthewwang.me>
2019-03-30 22:59:27 -0700

    adds blank state controller

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -6,28 +6,36 @@ class MessageList extends Component {
     messagesEnd = React.createRef()
 
     componentDidMount () {
-      this.scrollToBottom()
+        this.scrollToBottom()
     }
     componentDidUpdate () {
-      this.scrollToBottom()
+        this.scrollToBottom()
     }
     scrollToBottom = () => {
-      this.messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
+        this.messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
     }
-  render() {
-    return (
-      <div className="message-list">
-          {
-              this.props.messageList.map((object, i) => {
-                return (
-                    <Message messageObject={object} key={i}></Message>
-                )
-            })
-          }
-          <div ref={this.messagesEnd} />
-      </div>
-    );
-  }
+    render() {
+        if (this.props.messageList.length == 0){
+            return (
+                <div className="text-white">
+                    It's empty. Why not say something?
+                    <div ref={this.messagesEnd} />
+                </div>
+            )
+        }
+        return (
+            <div className="message-list">
+                {
+                    this.props.messageList.map((object, i) => {
+                        return (
+                            <Message messageObject={object} key={i}></Message>
+                        )
+                    })
+                }
+                <div ref={this.messagesEnd} />
+            </div>
+        );
+    }
 }
 
 export default MessageList;
