3049ee3114097b3e0f132297330aef7e7fef795c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-02-18 03:43:53 +0000

    Deploy malsf21/onechan to github.com/malsf21/onechan.git:gh-pages

---
+++ precache-manifest.3f74eab4ddfd4cb33b94eb19260f4668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9bdad5357b5e8791029",
+    "url": "/static/js/main.e93f5c7a.chunk.js"
+  },
+  {
+    "revision": "85627eb5c77867c3b7b3",
+    "url": "/static/js/2.a629ebca.chunk.js"
+  },
+  {
+    "revision": "e9bdad5357b5e8791029",
+    "url": "/static/css/main.fbe9d841.chunk.css"
+  },
+  {
+    "revision": "85627eb5c77867c3b7b3",
+    "url": "/static/css/2.aece9c69.chunk.css"
+  },
+  {
+    "revision": "b3dfdfe0510d5400e0b81b9c7ec52af5",
+    "url": "/index.html"
+  }
+];
